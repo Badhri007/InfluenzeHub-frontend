75e1fdf209076c2b6312c4fb61929ed073f57810
Badhri007 <badhrisambath2003@gmail.com>
2024-09-06 09:57:01 +0530

    updated search bar

--- src/components/sponsor/SearchBar.jsx
+++ src/components/sponsor/SearchBar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-
 const SearchBar = () => {
   return (
-    <div className=' flex flex-col'>
-        <select className='rounded-xl w-[40%] h-8  p-1 m-auto'>
-            <option>Electronics</option>
-            <option>Food</option>
-            <option>Clothing</option>
-            <option>Accessories</option>
-            <option>Other</option>
-        </select>
-        <br/>
-        <input className='w-[80%] h-8 rounded-xl p-2 flex items-center m-auto bg-white shadow-lg'/>
-        <br/>
-        
+    <div className='flex flex-col md:flex-row gap-1 md:gap-2  items-center justify-center'>
+      <select className='rounded-xl w-[40%] h-10 p-1 shadow-lg md:w-[20%] lg:w-[10%]'>
+        <option>Electronics</option>
+        <option>Food</option>
+        <option>Clothing</option>
+        <option>Accessories</option>
+        <option>Other</option>
+      </select>
+      <input
+        type="text"
+        placeholder="Search..."
+        className='w-[70%] h-10 rounded-xl p-2 flex  items-center bg-white shadow-lg lg:w-[30%]'
+      />
     </div>
   )
 }
