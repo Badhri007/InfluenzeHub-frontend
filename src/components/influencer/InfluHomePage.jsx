2a7d47bdcfbd6c719eccff77677e7acf1adce689
Badhri007 <badhrisambath2003@gmail.com>
2024-09-16 23:37:21 +0530

    Fixed UI

--- src/components/influencer/InfluHomePage.jsx
+++ src/components/influencer/InfluHomePage.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import maleIcon from '../../assets/male.jpg';
+import femaleIcon from '../../assets/female.jpg';
 const InfluHomePage = () => {
+
+  const [adrequest,setAdRequest]=useState({
+    
+  })
+
+  const [profile,setProfile]=useState({
+    'name':'',
+    'gender':'',
+    'niche':'',
+    'email':'',
+    'platform':''
+  })
+
+  const [requests,setRequests]=useState([]);
+
+
   return (
+    <div>
     <div>InfluHomePage</div>
+    <div className='md:ml-10 flex items-center justify-center md:justify-start' >
+    <img src={maleIcon} className='w-40 h-40 md:w-60 md:h-60 rounded-[50%]'/>
+    <p></p>
+    </div>
+    </div>
+
   )
 }
 
--- src/components/influencer/InfluRegister.jsx
+++ src/components/influencer/InfluRegister.jsx
@@ -9,11 +9,13 @@ const InfluRegister = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     'username': '',
+    'gender': '',
     'niche': '',
-    'reach': 0,
+    'reach': '',
     'email': '',
     'password': '',
-    'platform': ''
+    'platform': '',
+    'profile_photo_url': ''
   });
 
   const handleChanges = (e) => {
@@ -59,29 +61,73 @@ const InfluRegister = () => {
           <div className="hidden  lg:block w-[50%] ">
             <img src={background} className='h-[80%]' alt="background" />
           </div>
-          <div className="block w-full lg:w-[50%]">
-            <form className="flex flex-col justify-center items-center h-[80%] bg-gray-100" onSubmit={handleSubmit}>
+          <div className=" w-full bg-gray-100 lg:w-[50%]">
+            <br />
+            <p className="font-bold text-center text-xl mt-0 lg:text-xl">INFLUENCER REGISTRATION</p>
+            <form className="flex flex-col justify-center items-center h-[100%]" onSubmit={handleSubmit}>
+
               <br />
-              <p className="font-bold text-xl lg:text-2xl">INFLUENCER REGISTRATION</p>
-              <br />
-              <label>Username</label>
-              <input type="text" name="username" value={formData.username} className="rounded-lg shadow-lg w-1/3 h-8" onChange={handleChanges} />
-              <label>Niche</label>
-              <input type="text" name="niche" value={formData.niche} className="rounded-lg shadow-lg w-1/3 h-8" onChange={handleChanges} />
-              <label>Reach</label>
-              <input type="number" name="reach" value={formData.reach} className="rounded-lg shadow-lg w-1/3 h-8" onChange={handleChanges} />
-              <label>Email</label>
-              <input type="email" name="email" value={formData.email} className="rounded-lg shadow-lg w-1/3 h-8" onChange={handleChanges} />
-              <label>Password</label>
-              <input type="password" name="password" value={formData.password} className="rounded-lg shadow-lg w-1/3 h-8" onChange={handleChanges} />
-              <label>Platform Presence</label>
-              <select name="platform" value={formData.platform} className="rounded-lg shadow-lg w-1/3 h-8" onChange={handleChanges}>
-                <option value="" disabled>Select type</option>
-                <option value="Twitter">Twitter</option>
-                <option value="Youtube">YouTube</option>
-                <option value="Instagram">Instagram</option>
-                <option value="LinkedIn">LinkedIn</option>
-              </select>
+              <div className="grid grid-cols-1 md:grid-cols-2 w-full">
+                <div>
+                  <label className='ml-4'>Username</label>
+                  <br />
+                  <input type="text" name="username" value={formData.username} className="rounded-lg shadow-lg w-2/3 md:w-[80%] h-10 mb-3 ml-4 p-2" placeholder="Profile name" onChange={handleChanges} />
+                  <br />
+
+                  <br />
+                  <label className='ml-4'>Gender</label>
+                  <br />
+                  <div>
+                    <input type="radio" name="gender" id="gender" value="Male" onChange={handleChanges} className="mx-3 w-10 h-5 align-middle" checked={formData.gender === "Male"} />Male
+                    <br />
+                    <input type="radio" name="gender" id="gender" value="Female" onChange={handleChanges} className="mx-3 w-10 h-5 align-middle" checked={formData.gender === "Female"} />Female
+                    <br />
+                    <input type="radio" name="gender" id="gender" value="Others" onChange={handleChanges} className="mx-3 w-10 h-5 align-middle" checked={formData.gender === "Others"} />Others
+                    <br />
+                  </div>
+
+                  <br />
+                  <label className='ml-4'>Niche</label>
+                  <br />
+                  <select type="text" name="niche" value={formData.niche} className="rounded-lg bg-white shadow-lg w-2/3 h-10 mb-3 ml-4 p-2 md:w-[80%]" placeholder='Category' onChange={handleChanges}>
+                      <option value="">All Categories</option>
+                      <option value="Sports">Sports</option>
+                      <option value="Science">Science</option>
+                      <option value="History">History</option>
+                      <option value="Dance">Dance</option>
+                      <option value="Entertainment">Entertainment</option>
+                      <option value="Dance/Music">Dance/Music</option>
+                      <option value="Medicine">Medicine</option>
+                      <option value="Celebrities">Celebrities</option>
+                      <option value="Other">Other</option>
+                  </select>
+                  <br />
+                  <label className='ml-4'>Reach</label>
+                  <br />
+                  <input type="number" name="reach" value={formData.reach} className="rounded-lg shadow-lg w-2/3 h-10 mb-3 ml-4 p-2 md:w-[80%]" placeholder="Followers/Subscribers" onChange={handleChanges} />
+
+                </div>
+                <div>
+                  <label className='ml-4 md:ml-2'>Email</label>
+                  <br />
+                  <input type="email" name="email" value={formData.email} className="rounded-lg shadow-lg w-2/3 h-10 m-3 ml-4 md:ml-2 p-2 md:w-[80%]" onChange={handleChanges} />
+                  <br />
+                  <label className='ml-4 md:ml-2'>Password</label>
+                  <br />
+                  <input type="password" name="password" value={formData.password} className="rounded-lg shadow-lg w-2/3 md:w-[80%] ml-4 h-10 m-3 md:ml-2 p-2" onChange={handleChanges} />
+                  <br />
+                  <label className='ml-4 md:ml-2'>Platform Presence</label>
+                  <br />
+                  <select name="platform" value={formData.platform} className="rounded-lg shadow-lg w-2/3 h-10 m-3 ml-4 md:ml-2 p-2 md:w-[80%]" onChange={handleChanges}>
+                    <option value="" disabled>Select type</option>
+                    <option value="Twitter">Twitter</option>
+                    <option value="Youtube">YouTube</option>
+                    <option value="Instagram">Instagram</option>
+                    <option value="LinkedIn">LinkedIn</option>
+                  </select>
+                  <br />
+                </div>
+              </div>
               <br />
               <button className="bg-blue-500 rounded-xl p-2 mt-2 text-white" type="submit">Register</button>
               <br />
