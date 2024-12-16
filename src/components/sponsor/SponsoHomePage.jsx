81c90a3ccc052268e5fe989b979c4a2f20f29792
Badhri Narayanan <badhrisambath2003@gmail.com>
2024-12-16 21:59:08 +0530

    added sponsor stats

--- src/components/sponsor/SponsoHomePage.jsx
+++ src/components/sponsor/SponsoHomePage.jsx
@@ -5,6 +5,19 @@ const SponsoHomePage = () => {
   const [sponsorName, setSponsorName] = useState('Loading...');
   const [allCampaigns, setAllCampaigns] = useState([]);
   const [allAdsList, setAllAdsList] = useState([]);
+
+  const [pendingAds,setPendingAds] = useState([]);
+  const [acceptAds,setAcceptAds] = useState([]);
+  const [rejectedAds,setRejectedAds] = useState([]);
+
+  const [totalAmountSpent,setTotalAmountSpent] = useState(0);
+  const [totalBudget,setTotalBudget] = useState(0);
+
+  const [currentCampaignBudget,setCurrentCampaignBudget] = useState(0);
+
+  const [profitableCampaigns,setProfitableCampaigns] = useState([]);
+  const [failedCampaigns,setFailedCampaigns] = useState([]);
+
   const [error, setError] = useState(null);
 
   const headers = {
@@ -35,6 +48,19 @@ const SponsoHomePage = () => {
       if (response.ok) {
         const campaigns = await response.json();
         setAllCampaigns(campaigns);
+
+        let totalBudget = 0;
+
+        campaigns.map((campaign)=>{
+          totalBudget+=campaign.budget;
+          setCurrentCampaignBudget(campaign.budget);
+        })
+
+        console.log("Total Budget:",totalBudget);
+
+        setTotalBudget(totalBudget);
+
+
       } else {
         setError('Failed to fetch campaigns.');
       }
@@ -53,6 +79,17 @@ const SponsoHomePage = () => {
       if (response.ok) {
         const ads = await response.json();
 
+        // let adsCostForCurrentCampaign = 0;
+        // ads.map((ad,index)=>{
+        //   adsCostForCurrentCampaign+=ad.payment_amount;
+        // });
+
+        // if(adsCostForCurrentCampaign<currentCampaignBudget)
+        // {
+        //     setProfitableCampaigns()
+        // }
+
+
         setAllAdsList((prevAdsList) => {
           const combinedAds = [...prevAdsList, ...ads];
           const uniqueAds = Array.from(new Set(combinedAds.map((ad) => JSON.stringify(ad)))).map(
@@ -60,6 +97,7 @@ const SponsoHomePage = () => {
           );
           return uniqueAds;
         });
+
 
       } else {
         console.error('Failed to fetch ads for campaign', campaignId);
@@ -72,10 +110,40 @@ const SponsoHomePage = () => {
   const fetchAllAds = async () => {
     try {
       await Promise.all(allCampaigns.map((campaign) => getAdsForCurrentCampaign(campaign._id)));
+      filterOldUpcomingAds(allAdsList);
     } catch (error) {
       console.error('Error fetching all ads:', error);
     }
   };
+
+
+  const filterOldUpcomingAds=(allAdsList)=>{
+
+    // console.log(allAdsList);
+
+    let pending = allAdsList.filter(ad => ad.status ==="pending")
+    // console.log("Pending ads count:",pending.length);
+    setPendingAds(pending);
+
+    let accepted = allAdsList.filter(ad => ad.status ==="accept")
+    console.log("Accepted ads count:",accepted);
+    setAcceptAds(accepted);
+
+    let rejected = allAdsList.filter(ad => ad.status === "reject")
+    // console.log("Rejected Ad count:",rejected);
+    setRejectedAds(rejected);
+
+    let totalAmountSpent = 0;
+
+    acceptAds.map((ad)=>{
+      totalAmountSpent+=ad.payment_amount;
+    })
+
+    console.log("Total Amount Spent:",totalAmountSpent);
+    setTotalAmountSpent(totalAmountSpent);
+  }
+
+
 
   useEffect(() => {
     (async () => {
@@ -87,38 +155,79 @@ const SponsoHomePage = () => {
   useEffect(() => {
     if (allCampaigns.length > 0) {
       fetchAllAds();
+      // console.log(allAdsList);
+
     }
   }, [allCampaigns]);
 
   return (
-    <div className='bg-gray-200 w-full h-screen'>
+    <div className='bg-gray-200 w-full h-full'>
       <div className='p-4 w-full h-full'>
         <div className='p-4'>
         <h1 className='text-3xl ml-5'>Welcome, {sponsorName}!</h1>
         </div>
 
-        <div className='flex flex-row w-full h-[25%] justify-around'>
-          <div className='bg-white w-[20%] rounded-xl p-4 m-2'>
-            <p>Total Campaigns</p>
-            <p className='text-9xl'>{allCampaigns.length}</p>
+        <div className='sm:grid grid-cols-1 sm:w-full md:grid-cols-3  lg:flex flex-row w-full h-[25%] justify-around'>
+          <div className='sm:w-full md:w-[90%] lg:w-[15%]   bg-white  rounded-xl p-4 m-2'>
+            <p className='p-1 text-center bg-cyan-300 rounded-xl'>Total Campaigns</p>
+            <p className='text-9xl text-center'>{allCampaigns.length}</p>
           </div>
 
-          <div className='bg-white w-[20%] rounded-xl p-4 m-2'>
-            <p>Total Advertisements</p>
-            <p className='text-9xl'>{allAdsList.length}</p>
+          <div className='bg-white sm:w-full md:w-[90%] lg:w-[15%]  rounded-xl p-4 m-2'>
+            <p className='p-1 text-center bg-violet-300 rounded-xl'>Total Advertisements</p>
+            <p className='text-9xl text-center'>{allAdsList.length}</p>
           </div>
 
-          <div className='bg-white w-[20%] rounded-xl p-4 m-2'>
-            <p>Total Advertisements</p>
-            <p className='text-9xl'>{allAdsList.length}</p>
+          <div className='bg-white sm:w-full md:w-[90%] lg:w-[15%]  rounded-xl p-4 m-2'>
+            <p className='bg-green-400 p-1 text-center rounded-xl'>Total Accepted Ads</p>
+            <p className='text-9xl text-center'>{acceptAds.length}</p>
           </div>
 
-          <div className='bg-white w-[20%] rounded-xl p-4 m-2'>
-            <p>Total Advertisements</p>
-            <p className='text-9xl'>{allAdsList.length}</p>
+          <div className='bg-white sm:w-full md:w-[90%] lg:w-[15%]  rounded-xl p-4 m-2'>
+            <p className='bg-red-300 p-1 text-center rounded-xl'>Total Rejected Ads</p>
+            <p className='text-9xl text-center'>{rejectedAds.length}</p>
+          </div>
+
+          <div className='bg-white sm:w-full md:w-[90%] lg:w-[15%]  rounded-xl p-4 m-2'>
+            <p className='bg-yellow-300 p-1 text-center rounded-xl'>Total Pending Ads</p>
+            <p className='text-9xl text-center'>{pendingAds.length}</p>
           </div>
         </div>
 
+
+      </div>
+
+      <div>
+        <div className='flex flex-row justify-between p-[10%]'>
+          <div >
+            <div className='mb-3'>
+              <label className=' p-1 font-bold rounded-xl'>TOTAL BUDGET: </label>
+              <p className='text-3xl p-1.5 bg-white  rounded-xl'>${totalBudget}</p>
+            </div>
+
+            <div className='mb-3'>
+              <label className=' p-1 font-bold rounded-xl'>EXPENSES:  </label>
+              <p className='text-3xl p-1.5  bg-white  rounded-xl'>${totalAmountSpent}</p>
+            </div>
+
+            <div className='mb-3'>
+              <label className=' p-1 font-bold rounded-xl'>BALANCE: </label>
+              <p className='text-3xl p-1.5  bg-white  rounded-xl'>${totalBudget - totalAmountSpent}</p>
+            </div>
+
+          </div>
+          <div>
+            <p>Profitable Campaign:</p>
+            <p>Failed Campaign:</p>
+
+          </div>
+
+          <div>
+          <a href="#" className='underline'> Influencers Hired </a>
+          </div>
+
+
+        </div>
 
       </div>
       {/* <div>
